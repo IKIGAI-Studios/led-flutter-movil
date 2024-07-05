42d5825544ba3d1f35156e3f28db41333aa23cb9
Erick Pinzon <65203105+erickpinzon18@users.noreply.github.com>
2024-07-05 01:30:22 -0600

    Split fcm api

--- lib/api/firebase_api.dart
+++ lib/api/firebase_api.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:http/http.dart' as http;
 
 class FirebaseApi {
   // Instance of Firebase Messaging
@@ -20,49 +17,6 @@ class FirebaseApi {
     print('FCM Token: ${fcmToken.toString()}');
 
     initPushNotifications();
-  }
-
-  // Send a notification to a specific device
-  Future<void> changeLEDstatus(
-    bool status,
-  ) async {
-    // Validate if the server key is present
-    if (dotenv.env['SERVER_KEY'] == null) {
-      // ignore: avoid_print
-      return print('SERVER_KEY not found in .env file');
-    }
-    final String serverKey = dotenv.env['SERVER_KEY'] ?? '';
-
-    final Uri fcmUrl = Uri.parse('https://fcm.googleapis.com/fcm/send');
-
-    final headers = {
-      'Content-Type': 'application/json',
-      'Authorization': 'key=$serverKey',
-    };
-
-    final requestBody = jsonEncode({
-      'notification': {
-        'title': 'LED status changed',
-        'body': status ? 'LED is now ON' : 'LED is now OFF',
-      },
-      'priority': 'high',
-      'data': {
-        'click_action': 'FLUTTER_NOTIFICATION_CLICK',
-        'status': 'done',
-      },
-      'to': 'led/actions',
-    });
-
-    final response =
-        await http.post(fcmUrl, headers: headers, body: requestBody);
-
-    if (response.statusCode == 200) {
-      // ignore: avoid_print
-      print('Notificación enviada correctamente');
-    } else {
-      // ignore: avoid_print
-      print('Error al enviar notificación: ${response.body}');
-    }
   }
 
   // Handle notifications (Esto es pal wear asi que no se usa aqui)
